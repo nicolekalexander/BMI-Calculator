14a3e7138523e1ed92773573427c710a5595bdc7
Nicole Alexander <nkalexander1@verizon.net>
2020-01-29 13:25:22 -0500

    first commit

---
+++ bmi-calculator.js
@@ -0,0 +1,39 @@
+//function that calculates bmi from age, weight, and height
+const bmiCalculate = (height, weight) => { 
+    // return bmi 
+    return (weight/(height**2)) * 703   
+   
+}
+
+
+// function that determines weight class from bmi
+const getWeightClass = (bmi) => {
+     
+    if (bmi < 18.5) {
+        return "underweight"
+    } else if (bmi < 25) {
+        return "normal weight"
+    } else if (bmi < 30) {
+        return "overweight"
+    } else {
+        return "obese"
+    }
+}
+
+$("#userResults").hide()
+const urlParams = new URLSearchParams(window.location.search);
+const userHeight = urlParams.get('height')
+const userWeight = urlParams.get('weight')
+
+if (userHeight && userWeight) {
+    const userBMI = bmiCalculate(userHeight, userWeight)
+    console.log(userBMI)
+    const userWeightClass = getWeightClass(userBMI)
+    console.log(userWeightClass)
+
+    $("#userResults").show()
+    $("#userBMI").text(Math.round(userBMI), 2)
+    $("#userWeightClass").text(userWeightClass)
+    $("#userHeight").text(userHeight)
+    $("#userWeight").text(userWeight)
+}
